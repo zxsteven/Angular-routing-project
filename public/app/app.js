0ced00459eb1a48cd347f328dd8401e4e6e7be1f
Steven Buxton <breathesteve@gmail.com>
2016-01-04 15:19:16 -0600

    config routes

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,28 @@
-(function() {
+var app = angular.module('app', ['ngRoute'])
 
-    var app = angular.module('app', ['ngRoute']);
-
-    app.config(['$logProvider', function ($logProvider) {
-
-        $logProvider.debugEnabled(true);
-
-    }]);
-
-}());
+.config(['$logProvider', '$routeProvider', function ($logProvider, $routeProvider) {
+  $logProvider.debugEnabled(true);
+  
+  $routeProvider
+    .when('/', {
+      controller: 'HomeController',
+      controllerAs: 'home',
+      templateUrl: '/public/app/templates/home.html'
+  })
+    .when('/schools', {
+      controller: 'AllSchoolsController',
+      controllerAs: 'schools',
+      templateUrl: '/public/app/templates/allSchools.html'
+  })
+    .when('/classrooms', {
+      controller: 'AllClassroomsController',
+      controllerAs: 'classrooms',
+      templateUrl: '/public/app/templates/allClassrooms.html'
+  })
+    .when('/activities', {
+      controller: 'AllActivitiesController',
+      controllerAs: 'activities',
+      templateUrl: '/public/app/templates/allActivities.html'
+  })
+  .otherwise('/');
+}]);
